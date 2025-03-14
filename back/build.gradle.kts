2d0d8a5bd1f28a4c4ef2b89f2ece87dba68b7caf
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-03-14 20:20:36 +0900

    🎨 Style : formatter
    - `google-java-format-for-vs-code` for *.java
    - `ktlint` for *kts

--- back/build.gradle.kts
+++ back/build.gradle.kts
@@ -1,21 +1,21 @@
 plugins {
-	java
-	id("org.springframework.boot") version "3.5.0-M2"
-	id("io.spring.dependency-management") version "1.1.7"
+    java
+    id("org.springframework.boot") version "3.5.0-M2"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(23)
-	}
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(23)
+    }
 }
 
 repositories {
-	mavenCentral()
-	maven { url = uri("https://repo.spring.io/milestone") }
+    mavenCentral()
+    maven { url = uri("https://repo.spring.io/milestone") }
 }
 
 dependencies {
@@ -23,16 +23,16 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    
-	// Build web, including RESTful, applications using Spring MVC
-	// Uses Apache Tomcat as the default embedded container
+
+    // Build web, including RESTful, applications using Spring MVC
+    // Uses Apache Tomcat as the default embedded container
     implementation("org.springframework.boot:spring-boot-starter-web")
-    
+
     // Provides fast application restarts, LiveReload, and configurations
     developmentOnly("org.springframework.boot:spring-boot-devtools")
 
-	// Generate metadata for developers to offer contextual help and "code completion"
-	// when working with custom configuration keys (ex.application.properties/.yml files)
+    // Generate metadata for developers to offer contextual help and "code completion"
+    // when working with custom configuration keys (ex.application.properties/.yml files)
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
     // Java annotation library which helps to reduce boilerplate code
@@ -40,12 +40,12 @@ dependencies {
     annotationProcessor("org.projectlombok:lombok")
     testCompileOnly("org.projectlombok:lombok")
     testAnnotationProcessor("org.projectlombok:lombok")
-    
-	// Document RESTful services by combining hand-written with Asciidoctor
-	// and auto-generated snippets produced with Spring MVC Test
-	testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+
+    // Document RESTful services by combining hand-written with Asciidoctor
+    // and auto-generated snippets produced with Spring MVC Test
+    testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
