2602ac81c4182357255be598355c7a7528d3eae1
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-03-19 01:17:58 +0900

    🐛 Fix : duplicated QuestionAPITests

--- back/src/test/java/com/example/mistakes/api/questions/QuestionServiceTests.java
+++
@@ -1,9 +0,0 @@
-package com.example.mistakes.api.questions;
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class QuestionAPITests {
-
-  void testGetQuestion() throws Exception {}
-}
