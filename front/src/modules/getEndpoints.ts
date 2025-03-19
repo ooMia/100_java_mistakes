2d5d623246e8336355bb7b2018fbb885dc3a6a64
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-03-19 19:39:48 +0900

    ✨ Feat : refactor layouts, making components

--- front/src/modules/getEndpoints.ts
+++ front/src/modules/getEndpoints.ts
@@ -1,12 +1,23 @@
 export const endpoints: { subject: string; id: string }[] = [
+  // mistake global id
   {
     subject: "questions",
-    id: "t1",
+    id: "1",
   },
   {
     subject: "questions",
-    id: "t2",
+    id: "2",
   },
+  // chapter 2 local index
+  {
+    subject: "expression",
+    id: "1",
+  },
+  {
+    subject: "expression",
+    id: "2",
+  },
+  // legacy endpoint
   {
     subject: "questions",
     id: "t3",
